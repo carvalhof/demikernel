a2a643e101421d9c3414cdfea61971acb33a7dc2
carvalhof <carvalhof.br@gmail.com>
2024-06-24 21:54:59 -0400

    wip

--- run_server.sh
+++ run_server.sh
@@ -25,13 +25,13 @@ if [ ${USE_PERF} -eq 1 ]; then
 
     SERVER_LCORES_FOR_PERF=`echo ${SERVER_CPUS} | cut -d, -f2-`
 
-    taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu perf stat -C ${SERVER_LCORES_FOR_PERF} -A -I ${DELAY} -d -d -d -x'|' -o output.perf make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=tcp-server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT
-    #taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu perf record -e page-faults make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=tcp-server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT
+    taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu perf stat -C ${SERVER_LCORES_FOR_PERF} -A -I ${DELAY} -d -d -d -x'|' -o output.perf make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT
+    #taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu perf record -e page-faults make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT
 
     echo 1 | sudo tee /proc/sys/kernel/nmi_watchdog
 else
-    taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=tcp-server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT
-    #taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=tcp-server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT RUST_LOG=trace
+    taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT
+    #taskset -c ${SERVER_CPUS} sudo HOME=${HOMEDIR} LD_LIBRARY_PATH=$HOME/lib/x86_64-linux-gnu make test-system-rust CONFIG_PATH=${HOME}/config.yaml.novo LIBOS=catnip TEST=server_db ARGS="${ARGS}" TIMEOUT=$TIMEOUT RUST_LOG=trace
 fi
 
 ## Example to run:
